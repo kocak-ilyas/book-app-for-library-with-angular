909de96c86a47f32979c06bc206fc70533867a34
ilyas kocak <kocakilyas18@gmail.com>
2021-06-07 12:33:11 +0300

    env2

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book, Sign } from '../models/book';
-import { environment  } from '../../environments/environment.prod';
+import { environment  } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
